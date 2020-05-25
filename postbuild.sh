64b7991da58f4cb549eb272f8b0cf8ede7030ce7
azuki <git@azuki.vip>
2020-05-25 14:01:48 -0700

    update postbuild and remove custom vst3 folder

--- postbuild.sh
+++ postbuild.sh
@@ -1,1 +1,5 @@
-cp -r Builds/VisualStudio2017/x64/Release/VST3 . && cp -r external/spleeterpp/models/filter VST3/models && cp extras/tensorflow.dll VST3/
+cp -r Builds/VisualStudio2017/x64/Release/VST3 . && cp extras/tensorflow.dll VST3/
+curl -L -o filter.zip 'https://github.com/diracdeltas/spleeterpp/releases/download/v0.1/filter.zip'
+unzip filter.zip
+rm filter.zip
+mv filter VST3/models
